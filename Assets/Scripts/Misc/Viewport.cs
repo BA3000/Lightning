56b55884df28bfd6700ba16189941f8045f3259b
BA3000 <2864281096@qq.com>
2023-10-08 03:13:22 +0800

    add: functions to generate new enemy target position

--- Assets/Scripts/Misc/Viewport.cs
+++ Assets/Scripts/Misc/Viewport.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Viewport : Singleton<Viewport>
@@ -9,6 +6,7 @@ public class Viewport : Singleton<Viewport>
     private float _maxX;
     private float _minY;
     private float _maxY;
+    private float _midX;
 
     private void Start()
     {
@@ -21,6 +19,8 @@ public class Viewport : Singleton<Viewport>
         _minY = bottomLeft.y;
         _maxX = topRight.x;
         _maxY = topRight.y;
+
+        _midX = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0f, 0f)).x;
     }
 
     public Vector3 PlayerMovablePosition(Vector3 playerPosition, float paddingX, float paddingY)
@@ -29,6 +29,47 @@ public class Viewport : Singleton<Viewport>
 
         position.x = Mathf.Clamp(playerPosition.x, _minX + paddingX, _maxX - paddingX);
         position.y = Mathf.Clamp(playerPosition.y, _minY + paddingY, _maxY - paddingY);
+
+        return position;
+    }
+
+    public Vector3 RandomEnemySpawnPosition(float paddingX, float paddingY)
+    {
+        Vector3 position = Vector3.zero;
+        position.x = _maxX + paddingX;
+        position.y = Random.Range(_minY + paddingY, _maxY - paddingY);
+
+        return position;
+    }
+
+    /// <summary>
+    /// limit enmey moving only in the right half screen
+    /// </summary>
+    /// <param name="paddingX"></param>
+    /// <param name="paddingY"></param>
+    /// <returns></returns>
+    public Vector3 RandomRightHalfPosition(float paddingX, float paddingY)
+    {
+        Vector3 position = Vector3.zero;
+
+        position.x = Random.Range(_midX, _maxX - paddingX);
+        position.y = Random.Range(_minY + paddingY, _maxY - paddingY);
+
+        return position;
+    }
+
+    /// <summary>
+    /// move in whole screen randomly
+    /// </summary>
+    /// <param name="paddingX"></param>
+    /// <param name="paddingY"></param>
+    /// <returns></returns>
+    public Vector3 RandomEnemyMovePosition(float paddingX, float paddingY)
+    {
+        Vector3 position = Vector3.zero;
+
+        position.x = Random.Range(_minX + paddingX, _maxX - paddingX);
+        position.y = Random.Range(_minY + paddingY, _maxY - paddingY);
 
         return position;
     }
