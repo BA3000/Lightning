0cc3305370060a6558bbe8be032178f065569c16
BA3000 <2864281096@qq.com>
2023-10-08 03:19:24 +0800

    update: fix trail display problem

--- Assets/Scripts/Projectile/PlayerProjectile.cs
+++ Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -1,7 +1,19 @@
+using UnityEngine;
+
 namespace Projectile
 {
     public class PlayerProjectile : Projectile
     {
-    
+        TrailRenderer trail;
+
+        private void Awake()
+        {
+            trail = GetComponentInChildren<TrailRenderer>();
+        }
+
+        private void OnDisable()
+        {
+            trail.Clear();
+        }
     }
 }
