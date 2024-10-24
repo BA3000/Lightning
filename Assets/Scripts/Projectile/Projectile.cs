e9291a2880a302ec64f47b47f3ba56496b64be15
BA3000 <2864281096@qq.com>
2024-10-25 01:40:33 +0800

    update: add colliders, ch11 wip

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -5,7 +5,8 @@ namespace Projectile
 {
     public class Projectile : MonoBehaviour
     {
-
+        [SerializeField] GameObject hitVFX;
+        [SerializeField] float damage;
         [SerializeField] private float moveSpeed = 10f;
         [SerializeField] protected Vector2 moveDirection;
 
@@ -23,6 +24,16 @@ namespace Projectile
                 transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
 
                 yield return null;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.TryGetComponent<Character>(out var character))
+            {
+                character.TakeDamage(damage);
+                PoolManager.Release(hitVFX, collision.GetContact(0).point, Quaternion.LookRotation(collision.GetContact(0).normal));
+                gameObject.SetActive(false);
             }
         }
     }
